80ff042e3ffee81d9ef6db7c757deeb958022205
Ashan <ashanvimodh241@gmail.com>
2025-04-26 15:41:29 +0530

    Complete Create trip page

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SelectBudgetOptions, SelectTravelesList } from "@/constants/options";
 import React, { useState } from "react";
@@ -6,8 +7,8 @@ import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 function CreatTrip() {
   const [place, setPlace] = useState();
   return (
-    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
-      <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 my-10">
+      <h2 className="font-bold text-3xl">Tell us your travel preferences 🏕️🌴</h2>
       <p className="mt-3 text-gray-500 text-xl">
         Just provide some basic information, and our trip planner will generate
         a customized itinerary based on your preferences.
@@ -67,6 +68,10 @@ function CreatTrip() {
           </div>
         </div>
       </div>
+      <div className="mt-10 flex justify-end">
+      <Button>Generate Trip</Button>
+      </div>
+      
     </div>
   );
 }
