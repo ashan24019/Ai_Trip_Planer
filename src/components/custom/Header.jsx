8fcc6dda00c7dfb09499353a5ea0805070eda08d
Ashan <ashanvimodh241@gmail.com>
2025-04-26 08:08:55 +0530

    Home page complete till images

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
 
 function Header() {
   return (
-    <header className="flex items-center p-4 bg-white shadow-md">
-      <img src="/logo.png" alt="Logo" className="w-10 h-10 mr-1" />
-      <span className="text-xl font-semibold">AI Trip Planner</span>
-    </header>
-  )
+    <div className="p-2 shadow-sm flex justify-between items-center px-3">
+      <div className="flex items-center p-4 bg-white">
+        <img src="/logo.png" alt="Logo" className="w-10 h-10 mr-1" />
+        <span className="text-xl font-semibold">AI Trip Planner</span>
+      </div>
+      <Button>Sign In</Button>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
